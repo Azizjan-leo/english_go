cc16a318d2ad101fd4a201835b40c62f9d53418f
Sedokina <lll.allino4ka.lll@gmail.com>
2015-09-05 23:19:58 +0600

    reg

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Reg/Log in</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="main_div">
+			<?php include("view/reg.php"); ?>
+		</div>
+	</body>
+</html>

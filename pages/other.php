44656b615851f18dd80277167eaf08858934d6fd
Azizjan-leo <litpulla@mail.ru>
2015-09-06 18:14:45 +0400

    таблица регистрации

--- pages/other.php
+++ pages/other.php
@@ -4,12 +4,17 @@
 <html>
 	<head>
 		<title>Tutorial</title>
-		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" type="text/css" href="../style.css">
 	</head>
-	
 	<body>
+	<center><br><br><form  method="post" action="">
+				<table>
+					<tr><td><input type="text" name="login" placeholder="Name" required/></td></tr>
+					<tr><td><input type="password" name="password" placeholder="Password" required/></td></tr>
+					<tr><td><input type="submit" name="auth" value="ENTER"></td></tr>
+				</table>
+			  </form></center>
 <?php
-  session_start();
 	$_SESSION['check'] = TRUE;
 	echo "<a href='../index.php'>Back</a>";
 ?>

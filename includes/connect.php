2eb078cf730e6c6a97a2dcf2a26919fb5431bf47
Azizjan-leo <litpulla@mail.ru>
2015-09-18 20:33:55 +0600

    Работа с БД (фиксю кириллицу)

--- includes/connect.php
+++ includes/connect.php
@@ -1,4 +1,7 @@
 <?php
+	session_start();
+	header("content-type=text/html; charset=cp1251");
 	mysql_connect ("localhost","root","") or die(mysql_error());
     mysql_select_db ("test")or die(mysql_error());
+	mysql_query("SET NAMES utf8");
 ?>
--- pages/users_settings.php
+++ pages/users_settings.php
@@ -1,23 +1,26 @@
 <?php 
-	session_start();
+	include("../includes/connect.php")
 ?>
 <html>
 	<head>
 		<title>Settings</title>
-		<link rel="stylesheet" type="text/css" href="../style.css">
+		<link rel="stylesheet" type="text/css" href="../styles/style.css">
 	</head>
 	<body>
 		<p> Settings <br/> </p>
 		<?php
-			print
-				"<table>
-					<tr><td> First name </td><td>".$_SESSION['user_name']."</td></tr>
-					<tr><td> Last name </td><td>".$_SESSION['user_name']."</td></tr>
-					<tr><td> Age </td><td>".$_SESSION['user_name']."</td></tr>
-					<tr><td> Sex </td><td>".$_SESSION['user_name']."</td></tr>
-					<tr><td> Skil color </td><td>".$_SESSION['user_name']."</td></tr>
-					<tr><td> Hair length </td><td>".$_SESSION['user_name']."</td></tr>
-				 </table>";
+			if($_SESSION['check'] = TRUE){
+				print
+					"<table>
+						<tr><td> First name </td><td>".$_SESSION['user_name']."</td></tr>
+						<tr><td> Last name </td><td>".$_SESSION['user_name']."</td></tr>
+						<tr><td> Age </td><td>".$_SESSION['user_name']."</td></tr>
+						<tr><td> Sex </td><td>".$_SESSION['user_name']."</td></tr>
+						<tr><td> Skil color </td><td>".$_SESSION['user_name']."</td></tr>
+						<tr><td> Hair length </td><td>".$_SESSION['user_name']."</td></tr>
+					 </table>";
+				print "<a href='../index.php'>Main page</a>";					 
+			}
 		?>
 	</body>
 </html>

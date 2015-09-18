a8bfeaa69598de23b3b966dc35c5035dfdb20155
Sedokina <lll.allino4ka.lll@gmail.com>
2015-09-18 22:09:48 +0600

    скрипт

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ function checkPass() {
 with(document)
 	//Проверка полей ввода пароля на соответствие
 	if (getElementById('pass').value.length != 0 || getElementById('passch').value.length != 0){
-		if(getElementById('passch').value.length > 0){
+		if(getElementById('pass').value.length == getElementById('passch').value.length){
 		getElementById('info').innerHTML = (getElementById('pass').value != getElementById('passch').value) ?
 		'<img src="../image/no.png">' : (document.getElementById("pass").value.length==document.getElementById("passch").value.length) ?
 		'<img src="../image/ok.png">' : '<img src="../image/no.png">';}

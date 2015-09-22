8cd4ad0162d9ca8de78413d6de6cd53337a522eb
Sedokina <lll.allino4ka.lll@gmail.com>
2015-09-22 22:15:49 +0600

    Navigation menu

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 ?>
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
 		<title>English-go</title>
 		<link rel="stylesheet" type="text/css" href="styles/style.css">
 	</head>
@@ -24,13 +25,44 @@
 			}
 			else{
 				echo $_SESSION["user_name"];
-				print '<form  method="post">
+				print '<div id="header"><center>HEADER</center></div>
+				<div id="content">
+				<div id="navigation">
+					<div class="nav_menu">
+						<ul id="menu">
+							<li>
+								<a href="index.php"><div>Дом<span>дом, любимый дом!</span></div></a>
+								<ul id="gradient_bg">
+									<li><a href="pages/kitchen.php">Кухня</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#"><div>Развлечения<span>все веселье тут!</span></div></a>
+								<ul id="gradient_bg">
+									<li><a href="pages/cinema.php">Кинотеатр</a></li>
+									<li><a href="pages/zoo.php">Зоопарк</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="#"><div>Менюшка<span>и тут можно написать</span></div></a>
+							</li>
+						</ul>
+					</div>
+					
+					<div class="user_stat">
+					Тут будет пользовательский интерфейс, завтра :D
+					</div>
+					
+				</div>
+				<div id="gen_content">
+				<form  method="post">
 				<input type="submit" name="out" value="OUT">
 				</form>
-				<a href="pages/kitchen.php">Kitchen</a><br/>
-				<a href="pages/zoo.php">Zoo</a><br/>
-				<a href="pages/cinema.php">Cinema</a><br/>';
-			  
+				</div>
+				</div>
+				<div id="footer">Footer</div>
+				';
+				
 			}
 				
 		?>

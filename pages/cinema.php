264bd683478b653375ae5011ac81dc8b15126c0e
Sedokina <lll.allino4ka.lll@gmail.com>
2015-09-27 01:04:18 +0600

    Кнопка для открытия формы, + фиксл меню

--- pages/cinema.php
+++ pages/cinema.php
@@ -18,33 +18,48 @@ if(!$_SESSION['check'])
 	echo '<script type="text/javascript">window.location.href="signInUp.php"</script>'; // если вошел не авторизованный чел его перенаправит на индекс
 else{
 	print '
-		<div id="navigation">
-		<div class="nav_menu">
-			<ul id="menu">
-				<li>
-					<a href="../index.php"><div>Дом<span>дом, любимый дом!</span></div></a>
-				<ul id="gradient_bg">
-				<li><a href="pages/kitchen.php">Кухня</a></li>
-				</ul></li><li>
-				<a href="#"><div>Развлечения<span>все веселье тут!</span></div></a>
-				<ul id="gradient_bg">
-				<li><a href="cinema.php">Кинотеатр</a></li>
-				<li><a href="pages/zoo.php">Зоопарк</a></li>
-				</ul>
-				</li>
-				<li>
-				<a href="#"><div>Менюшка<span>и тут можно написать</span></div></a>
-				</li>
-			</ul>
-		</div>	
-		<div class="user_stat">
-			<div class="user_data">
-				<div class="left_pos"><br><div class="user_name">'.$_SESSION["user_name"].'</div>
-					<div class="user_money">money</div></div>
-					<div class="right_pos"><div class="user_image"><img src="image/user_no_photo_big.png"></div></div>
-				</div>
-			</div>
-		</div>
+<div id="navigation">
+						<div class="nav_menu">
+							<ul id="menu">
+								<li>
+									<a href="../index.php"><div>Дом<span>дом, любимый дом!</span></div></a>
+									<ul id="gradient_bg">
+										<li><a href="../pages/kitchen.php">Кухня</a></li>
+									</ul>
+								</li>
+								<li>
+									<a href="#"><div>Развлечения<span>все веселье тут!</span></div></a>
+									<ul id="gradient_bg">
+										<li><a href="../pages/cinema.php">Кинотеатр</a></li>
+										<li><a href="../pages/zoo.php">Зоопарк</a></li>
+									</ul>
+								</li>
+								<li>
+									<a href="#"><div>Менюшка<span>и тут можно написать</span></div></a>
+								</li>
+							</ul>
+						</div>
+						
+						<div class="user_stat">
+							<div class="user_data">
+								<div class="left_pos"><br><div class="user_name">'.$_SESSION["user_name"].'</div>
+								<div class="user_money">money</div></div>
+								<div class="right_pos">
+									<div id="user_nav">
+									<img src="../image/user_no_photo_big.png">
+											<div id="down_menu">
+												<div class="triangle"></div>
+												<ul>
+													<li><a href="../pages/users_settings.php">Настройки</a></li>
+													<li><a href="?out">Выход</a></li>
+												</ul>
+											</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					</div>
 			<div id="content">
 			<div id="gen_content">';	
 	if(isset($_POST["send-request"])){
@@ -67,11 +82,13 @@ else{
 				}while($data = mysql_fetch_array($result));
 			}
 			
-	echo '<div id="video_form"><center><br><br><form method="POST" action="" enctype="multipart/form-data"><table><fieldset>
+	echo '<div id="video_form" style="display:none;"><center><br><br><form method="POST" action="" enctype="multipart/form-data"><table><fieldset>
 					<tr><td><input type="file" name="file" multiple accept="video/*" required/></td></tr>
 					<tr><td><input type="text" name="title" placeholder="| NAME" required/></td></tr>
 					<tr><td><input type="submit" name="send-request" value="Upload" /></td></tr>
-				  </table></fieldset></form></center></div>';
+				  </table></fieldset></form></center></div>
+				  <center><br><br><button onClick="HideShow(\'video_form\')">Add new video</button></center>
+				  ';
 	print '
 		
 		</div>

34c89f15396a399400c7e307a20c39f3f9bd338e
Azizjan-leo <litpulla@mail.ru>
2015-09-28 20:08:16 +0600

    Добавил постер к видео Добавил возможность указания категории загружаемого видео +ОБНОВЛЯЮ ДАМП !!!

--- pages/cinema.php
+++ pages/cinema.php
@@ -61,12 +61,14 @@ else{
 						
 					</div>
 			<div id="content">
-			<div id="gen_content">';	
+			<div id="gen_content">';
+			echo $files['section'];
 	if(isset($_POST["send-request"])){
 			function Uploading($files){
-				move_uploaded_file($files['tmp_name'], './video/'.$files['name']); 
+				move_uploaded_file($files['tmp_name'], '../videos/'.$files['name']); 
 				$f = $files['name'];
-				$query="INSERT INTO videos VALUES(NULL,'".$f."')";
+				$s = $_POST['section'];
+				$query="INSERT INTO videos VALUES(NULL,'".$f."','".$s."')";
 				mysql_query( $query ) or die(mysql_error());
 			}
 			Uploading($_FILES['file']);	
@@ -76,15 +78,15 @@ else{
 	if($data>0){
 		do{
 			printf('
-					<video height="400" width="600" controls="controls" poster="">
-						<source src="video/%s">
+					<video height="400" width="600" controls="controls" poster="../image/cinema_poster.jpg">
+						<source src="../videos/%s">
 					</video><br>',$data["name"]);
 				}while($data = mysql_fetch_array($result));
 			}
 			
 	echo '<div id="video_form" style="display:none;"><center><br><br><form method="POST" action="" enctype="multipart/form-data"><table><fieldset>
 					<tr><td><input type="file" name="file" multiple accept="video/*" required/></td></tr>
-					<tr><td><input type="text" name="title" placeholder="| NAME" required/></td></tr>
+					<tr><td><input type="text" name="section" placeholder="| Section" required/></td></tr>
 					<tr><td><input type="submit" name="send-request" value="Upload" /></td></tr>
 				  </table></fieldset></form></center></div>
 				  <center><br><br><button onClick="HideShow(\'video_form\')">Add new video</button></center>
